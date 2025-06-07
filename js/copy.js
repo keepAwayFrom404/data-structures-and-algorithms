ceb3809a29154e60398b12f2f8c74ee20aa29222
keepAwayFrom404 <xff777@qq.com>
2025-06-07 22:22:45 +0800

    feat（js）：copy function

--- js/copy.js
+++ js/copy.js
@@ -29,10 +29,16 @@ const deepClone = function(obj) {
       const it = obj[key];
       if(isAny(it).includes('Array') || isAny(it).includes('Object') ){ // 处理数组
         res[key] = deepClone(obj[key])
-      } else if(isAny(it).includes('Function')) { // 处理正则表达式
-
-      } else if(isAny(it).includes('Function')) { // 处理函数
-        
+      } else if(isAny(it).includes('RegExp')) { // 处理正则表达式
+        res[key] = new RegExp(obj[key])
+      } else if(isAny(it).includes('Function')) { // 处理函数：原型和闭包会丢失
+        const temp = obj[key].toString()
+        res[key] = new Function(`return ${temp}`)()
+        for(let it in obj[key]) {
+          if(obj[key].hasOwnProperty(it)) {
+            res[key][it] = obj[key][it] // 复制函数属性
+          }
+        }
       } else { // 基础类型直接返回
         res[key] = obj[key]
       }
@@ -40,12 +46,24 @@ const deepClone = function(obj) {
   }
   return res
 }
-
-const a1 = [1,{b: 2, d: {
-  e: 3
-}}]
+const c = 1
+const a1 = {
+  foo: function(a, b) {
+    return a + b + c
+  },
+  bar: [1,2,3,{a: 4}],
+  name: 'cadenli',
+  reg: /^cadenli/,
+}
+a1.foo.test = 'foo'
+a1.foo.test2 = {}
 
 const b1 = deepClone(a1)
+b1.bar.a = 3
+console.log(a1);
+console.log(b1);
+console.log(a1.foo.test2 === b1.foo.test2); // 对象属性需要特殊处理
+// console.log(b1.foo(2,3)); // 闭包丢失
 // b1[1].d.e = 4
 // console.log(b1,'b1');
 // console.log(a1);
